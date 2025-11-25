94100ccd3a2b48c7e43400fed0090930d352a35c
alexeytaratenko <alex.taratenko@gmail.com>
2025-11-25 17:52:52 +0300

    delete saving points in the list (local) + better db interaction logic

--- src/main/java/org/example/web3/beans/ListOfPointsBean.java
+++ src/main/java/org/example/web3/beans/ListOfPointsBean.java
@@ -1,12 +1,15 @@
 package org.example.web3.beans;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
+import java.util.List;
+
 import org.example.web3.entities.Point;
+import org.example.web3.service.PointService;
 
 @ManagedBean(name = "listOfPointsBean")
 @ApplicationScoped
@@ -16,23 +19,16 @@ public class ListOfPointsBean {
     @PersistenceContext(unitName = "pgPU")
     private EntityManager em;
 
-    private ArrayList<Point> points;
+    @EJB
+    private PointService pointService;
 
     @PostConstruct
     public void init() {
-        loadPointsFromDatabase();
+        getPoints();
     }
 
-    private void loadPointsFromDatabase() {
-        points = new ArrayList<>(em.createQuery("SELECT p FROM Point p", Point.class).getResultList());
-    }
-
-    public ArrayList<Point> getPoints() {
-        return points;
-    }
-
-    public void addPoint(Point p) {
-        points.add(p);
+    public List<Point> getPoints() {
+        return pointService.getAllPoints();
     }
 
 }
--- src/main/java/org/example/web3/beans/PointBean.java
+++ src/main/java/org/example/web3/beans/PointBean.java
@@ -1,77 +1,33 @@
 package org.example.web3.beans;
 
-import org.example.web3.entities.Point;
-import org.example.web3.hitCheck.managers.HitChecker;
 import org.example.web3.service.PointService;
 
 import javax.ejb.EJB;
-import javax.el.MethodExpression;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.List;
 
 @ManagedBean(name = "pointBean")
 @ViewScoped
 public class PointBean {
 
-    @ManagedProperty(value = "#{listOfPointsBean}")
-    private ListOfPointsBean listOfPointsBean;
-
     private BigDecimal x;
     private BigDecimal y;
     private BigDecimal r = BigDecimal.valueOf(1);
     private boolean hit;
-
     private BigDecimal canvasX;
     private BigDecimal canvasY;
-
 
     @EJB
     private PointService pointService;
 
-    public void savePoint() {
-        BigDecimal roundedX = x.setScale(2, RoundingMode.HALF_UP);
-        BigDecimal roundedY = y.setScale(2, RoundingMode.HALF_UP);
-
-        Point point = new Point(roundedX, roundedY, r, hit);
-        pointService.addPoint(point);
+    public void checkAndSavePoint() {
+        hit = pointService.checkAndSaveFromForm(x, y, r);
     }
 
-    public void savePointFromCanvas() {
-        BigDecimal roundedCanvasX = canvasX.setScale(2, RoundingMode.HALF_UP);
-        BigDecimal roundedCanvasY = canvasY.setScale(2, RoundingMode.HALF_UP);
-
-        Point point = new Point(roundedCanvasX, roundedCanvasY, r, hit);
-        pointService.addPoint(point);
-    }
-
-    public void checkPoint() {
-
-        HitChecker hitChecker = new HitChecker(x, y, r);
-        hit = hitChecker.check();
-
-        Point newPoint = new Point(x, y, r, hit);
-        listOfPointsBean.addPoint(newPoint);
-
-        savePoint();
-
-    }
-
-    public void checkPointFromCanvas() {
-
-        HitChecker hitChecker = new HitChecker(canvasX, canvasY, r);
-        hit = hitChecker.check();
-
-        Point newPoint = new Point(canvasX, canvasY, r, hit);
-        listOfPointsBean.addPoint(newPoint);
-
-         savePointFromCanvas();
-
+    public void checkAndSaveFromCanvas() {
+        hit = pointService.checkAndSaveFromCanvas(canvasX, canvasY, r);
     }
 
     public BigDecimal getX() { return x; }
@@ -85,33 +41,6 @@ public class PointBean {
 
     public boolean getHit() { return hit; }
     public void setHit(boolean hit) { this.hit = hit; }
-
-    public ListOfPointsBean getListOfPointsBean() {
-        return listOfPointsBean;
-    }
-
-    public void setListOfPointsBean(ListOfPointsBean listOfPointsBean) {
-        this.listOfPointsBean = listOfPointsBean;
-    }
-
-    public String getPointsJSON() {
-        List<Point> points = listOfPointsBean.getPoints();
-        StringBuilder json = new StringBuilder("[");
-
-        for (int i = 0; i < points.size(); i++) {
-            Point p = points.get(i);
-            json.append("{")
-                    .append("\"x\":").append(p.getX()).append(",")
-                    .append("\"y\":").append(p.getY()).append(",")
-                    .append("\"r\":").append(p.getR()).append(",")
-                    .append("\"hit\":").append(p.getHit())
-                    .append("}");
-            if (i < points.size() - 1) json.append(",");
-        }
-        json.append("]");
-
-        return json.toString();
-    }
 
     public void setCanvasX(BigDecimal canvasX) { this.canvasX = canvasX; }
     public BigDecimal getCanvasX() { return canvasX; }
